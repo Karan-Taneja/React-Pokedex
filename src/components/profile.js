159af77a44a9dee9429ad59c65639f69b4c0cb32
Karan Taneja <karantaneja@pursuit.org>
2019-02-11 00:52:26 -0500

    Initalized repo, added necessary modules and set up basic file hierarchy

---
+++ src/components/profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './profile.css'
+import Moves from './moves'
+
+const Profile = (props) => {
+
+}
+
+export default Profile;
---
+++ src/components/search.js
@@ -0,0 +1,12 @@
+import {React, Component} from 'react';
+import './search.css'
+
+class Search extends Component {
+
+    state = {
+        
+    }
+
+}
+
+export default Search;
