5249559669101cbdd3e10dec3999489a49f9628b
Karan Taneja <karantaneja@pursuit.org>
2019-02-13 15:02:58 -0500

    Implemented profile page and modified app.js to include it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import List from './containers/list';
+import Profile from './components/profile';
 
 class App extends Component {
 
   state = {
-    
+    activeView: 0,
+    pokemon:{},
+  }
+
+  toggleState = () => {
+    const {activeView} = this.state
+    if(activeView === 0) this.setState({activeView: 1})
+    else this.setState({activeView: 0})
+  }
+
+  sendPokemon = (data) => {
+    this.setState({pokemon:data})
   }
 
   render() {
+
+    const {activeView} = this.state
+
     return (
-      <div className="App">
-        We'll figure this out later
-      </div>
+       <>
+        <div>Search bar</div>
+        <hr/>
+        {activeView === 0 ?
+              <List toggle={this.toggleState} sendPokemon={this.sendPokemon}/> :
+              <Profile toggle={this.toggleState} pokemon={this.state.pokemon} sendPokemon={this.sendPokemon}/>}
+      </>
     );
   }
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './profile.css';
-import Move from './move';
-import Type from './type';
+import Movelist from '../containers/movelist';
+import Typelist from '../containers/typelist';
+import Sprites from '../containers/sprites';
+import Stats from '../containers/stats';
 import Axios from 'axios';
 
 
-const Profile = (props) => {
-    return <Type type="Grass"/>
+class Profile extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name: "",
+            type: [],
+            sprites: [],
+            stats:[],
+            moves:[],
+            modal:{
+                display:false,
+                data:{}
+            },
+        }
+    }
+
+    onClickMove = (e) => {
+        Axios.get(`https://pokeapi.co/api/v2/move/${e.target.getAttribute('value')}`)
+            .then((data) => {
+                console.log(data.request.response)
+            })
+            .then(() => {
+
+            })
+    }
+
+    toggleModal = () => {
+        const nuModal = {...this.state.modal}
+
+        if(nuModal.display) nuModal.display = false
+        else nuModal.display = true
+        
+        this.setState({modal: nuModal})
+    }
+
+    sendMoveData = (data) => {
+        this.setState({modal:data})
+    }
+
+    render(){
+
+        const pkmn = this.props.pokemon
+
+        return <>
+            <div>
+                <div>
+                    <div className="d-flex center">
+                        <div>
+                            <h4># {pkmn.id} - {pkmn.name}</h4>
+                            <img src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pkmn.id}.png`}/>
+                            <Typelist data={pkmn.types}/>
+                        </div>
+                        <Sprites data={pkmn.sprites}/>
+                    </div>
+                </div>
+            </div>
+            <div className="center d-flex">
+                <Stats data={pkmn.stats}/>
+            </div>
+            <Movelist data={pkmn.moves} sendMoveData={this.sendMoveData} onClickMove={this.onClickMove}/>
+        </>
+    }
+
 }
+
 
 export default Profile;
