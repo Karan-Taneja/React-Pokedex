6bef97783dcd96b1706a7b915511d36ff05085a6
Karan Taneja <karantaneja@pursuit.org>
2019-02-19 03:36:15 -0500

    Added placeholder navbar, implemented home button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from './containers/list';
 import Profile from './components/profile';
+import './App.css'
 
 class App extends Component {
 
@@ -16,6 +17,12 @@ class App extends Component {
     else this.setState({activeView: 0})
   }
 
+  returnHome = () => {
+    const {activeView} = this.state
+    if(activeView !== 0) this.setState({activeView: 0})
+    else return;
+  }
+
   sendPokemon = (data) => {
     this.setState({pokemon:data})
   }
@@ -26,8 +33,12 @@ class App extends Component {
 
     return (
        <>
-        <div>Search bar</div>
-        <hr/>
+        <nav className="navbar bg-danger justify-content-between">
+          <a href="#" className="navbar-brand" onClick={this.returnHome} style={{'color':'white', 'font-weight':'700'}}>Home</a>
+          <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+          </form>
+        </nav>
         {activeView === 0 ?
               <List toggle={this.toggleState} sendPokemon={this.sendPokemon}/> :
               <Profile toggle={this.toggleState} pokemon={this.state.pokemon} sendPokemon={this.sendPokemon}/>}
